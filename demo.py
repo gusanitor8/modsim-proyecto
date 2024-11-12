39ae17cfcd1cc6ac59b94ba112fc0459f12b01c9
LeivaDiego <lei21752@uvg.edu.gt>
2024-11-12 14:23:56 -0600

    Maze Solver DEMO

--- demo.py
+++ demo.py
@@ -1,20 +1,18 @@
 import pygame
 from maze import MazeGenerator
-from maze_drawer import matrix_to_image
 from path_generator import PathGenerator
 from genetic_algorithm import GeneticAlgorithm
-from gen_functions import fitness, mutation, crossover
+from gen_functions import fitness_explore, mutation, crossover
 from animator import MazeSolver
 
 EPOCHS = 1000
 POPULATION = 50
-MUTATION_RATE = 0.1
+MUTATION_RATE = 0.5
 
 if __name__ == "__main__":
 
-    #random.seed(42)
     maze_gen = MazeGenerator(50,50)   
-    maze_dic = maze_gen.get_matrix_dic("test_mazes/test1")
+    maze_dic = maze_gen.get_matrix_dic("mazes/my_dict.pkl")
 
     # Cargar la data del laberinto desde el archivo
     maze_matrix = maze_dic["matrix"]
@@ -22,19 +20,16 @@ if __name__ == "__main__":
     exit_position = maze_dic["exit"]
     checkpoints = maze_dic["checkpoints"]
 
-    image = matrix_to_image(maze_matrix, entry_position, exit_position, checkpoints=checkpoints)
-    image.show()
-    print()
-
     # Generar una población de caminos inicial
     path_gen = PathGenerator(maze_matrix, entry_position)
     path_gen.generate_paths(POPULATION)
     paths = path_gen.get_paths()
 
     # Aplicar el algoritmo genético para encontrar la solución
-    genetic = GeneticAlgorithm(paths, maze_dic, MUTATION_RATE, EPOCHS, POPULATION, fitness, crossover, mutation)
+    genetic = GeneticAlgorithm(paths, maze_dic, MUTATION_RATE, EPOCHS, POPULATION, fitness_explore, crossover, mutation)
     generations, fitness_history, solution_found, final_epoch = genetic.run()
-    print("solution_found!")
+    if solution_found:
+        print("solution_found!")
 
     # Mostrar solución en una ventana de Pygame
     pygame.init()
